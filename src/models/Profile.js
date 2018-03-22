868334e4983d46e9765aa60ef9bfdd334b8f8466
Andrei Schukin <schukin_andrei@mail.ru>
2018-03-23 00:53:45 +0600

    Update Profile model

--- src/models/Profile.js
+++ src/models/Profile.js
@@ -14,16 +14,38 @@ module.exports = function (sequelize) {
       }
     },
     firstName: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: false
     },
     lastName: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    birthdate: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    gender: {
+      type: Sequelize.ENUM,
+      values: ['male', 'female', 'other', 'rather not say'],
+      allowNull: false
+    },
+    country: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    city: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    avatar: {
+      type: Sequelize.STRING,
+      allowNull: true
     }
   },
   {
     underscored: true,
-    tableName: 'profiles',
-    freezeTableName: true
+    tableName: 'profiles'
   });
 
   return Profile;
