5491d8dfaa532b4ba4665aa4309c75053322523c
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-01 13:59:29 +0600

    Update posts routing
    Add POST end-point for create new post

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const Router = require('koa-router');
-const postsController = require('../controllers/post');
+const postController = require('../controllers/post');
 
 const authCheck = require('../middlewares/auth-check');
+const parseForm = require('../middlewares/parse-form');
+const validPostContent = require('../middlewares/valid-post-content');
 
 module.exports = new Router()
-  .get('/:id', authCheck, postsController.getPostById);
+  .get('/:id', authCheck, postController.getPostById)
+  .post('/', authCheck, parseForm, validPostContent, postController.createNewPost);
