f4be0afc6def2721b03cce5d8487b70d95661ee3
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-08 00:26:01 +0600

    Add component Footer

---
+++ front/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { range } from 'lodash';
+import '../styles/components/Footer.css';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className='footer'>
+        Footer
+      </div>
+    );
+  }
+}
+
+export default Footer;
