8b82858e4cd48a01ab8361a207d024b32abae342
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-08 00:44:46 +0600

    Update index.js
    Configure store and change main component to Root

--- front/src/index.js
+++ front/src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
+import rootReducer from './reducers';
 import App from './components/App';
+import Root from './containers/Root';
+
+import { createStore } from 'redux';
+
+const configureStore = preloadedState => createStore(
+  rootReducer,
+  preloadedState
+);
+
+const store = configureStore();
 
 render(
   <div>
-    <App/>
-  </div>, document.getElementById('app')
+    <Root store={store} />
+  </div>, document.getElementById('root')
 );
