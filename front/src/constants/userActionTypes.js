b1e9f17438ac017be4a8a74801e7c1a6c8a463d4
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-11 22:24:45 +0600

    Add new types of user actions

--- front/src/constants/userActionTypes.js
+++ front/src/constants/userActionTypes.js
@@ -1,3 +1,3 @@
-export const userActionTypes = {
-  GET_USER: 'GET_USER'
-}
+export const USER_GET_REQUEST = 'USER_GET_REQUEST';
+export const USER_GET_SUCCESS = 'USER_GET_SUCCESS';
+export const USER_GET_FAILED = 'USER_GET_FAILED';
