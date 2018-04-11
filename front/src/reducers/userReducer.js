cd6733fe58a13c1eb2e0a6334d0688cc152f3b60
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-11 22:19:11 +0600

    Update userReducer

--- front/src/reducers/userReducer.js
+++ front/src/reducers/userReducer.js
@@ -1,12 +1,23 @@
-import { userActionTypes } from '../constants/userActionTypes';
-const { GET_USER } = userActionTypes;
+import { USER_GET_REQUEST, USER_GET_SUCCESS } from '../constants/userActionTypes';
 
-export function userReducer(state = {}, action) {
+import { merge } from 'lodash';
+
+const initialState = {
+  user: {},
+  userData: {},
+  payload: {},
+  isFetching: true,
+  full: false
+}
+
+export default function userReducer(state = initialState, action) {
   const { type, payload } = action;
 
   switch(type) {
-    case GET_USER:
-      return { ...state, user: payload };
+    case USER_GET_REQUEST:
+      return { ...state, payload, isFetching: true };
+    case USER_GET_SUCCESS:
+      return { ...state, payload, isFetching: false };
     default:
       return state;
   }
