1dff1f7c02f87f7c55e0c06776a337821ec7c2ab
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-11 22:49:01 +0600

    Add saga for user

---
+++ front/src/sagas/user.js
@@ -0,0 +1,24 @@
+import { call, put, takeLatest } from 'redux-saga/effects';
+import { getUserSuccess, getUserFailed } from '../actions/userActions';
+import UserService from '../services/UserService';
+
+import { USER_GET_REQUEST,
+         USER_GET_SUCCESS,
+         USER_GET_FAILED } from '../constants/userActionTypes';
+
+const userService = new UserService();
+
+function* fetchUser(action) {
+  try {
+    const user = yield call(userService.getUser, action.payload);
+    yield put(getUserSuccess({ status: user.status, data: user.data }));
+  } catch(err) {
+    yield put(getUserFailed(err.message));
+  }
+}
+
+function* watchGetUserSaga() {
+  yield takeLatest(USER_GET_REQUEST, fetchUser);
+}
+
+export default watchGetUserSaga;
