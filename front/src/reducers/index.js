3bd97b90516f4ec15d14258889289002eade6044
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-13 16:00:25 +0600

    Add form reducer from 'redux-form'

--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 import userReducer from './userReducer';
 import postReducer from './postReducer';
@@ -7,7 +8,8 @@ import authReducer from './authReducer';
 const rootReducer = combineReducers({
   auth: authReducer,
   user: userReducer,
-  post: postReducer
+  post: postReducer,
+  form: formReducer
 });
 
 export default rootReducer;
