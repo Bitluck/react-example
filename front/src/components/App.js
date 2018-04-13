f49b9234577343f7f7e0532ebac203dba4ce52fa
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-13 15:53:56 +0600

    Add component Header with style

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../styles/components/App.css';
 import Main from './Main';
+import Header from './Header';
 import Footer from './Footer';
 
 class App extends React.Component {
   render() {
     return (
       <div className='app'>
+        <Header />
         <Main />
         <Footer />
       </div>
---
+++ front/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { range } from 'lodash';
+import '../styles/components/Header.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className='header'>
+        Header
+      </div>
+    );
+  }
+}
+
+export default Header;
