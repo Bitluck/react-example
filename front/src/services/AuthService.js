80fd8b07974d357b5cd88007ebdd58dfb2c2a67a
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-13 15:49:19 +0600

    Add AuthService

---
+++ front/src/services/AuthService.js
@@ -0,0 +1,10 @@
+import { get, post, put, del } from './BaseService';
+
+export default class AuthService {
+  async login(loginData) {
+    const response = await post(`/auth/signin`, loginData);
+    const res = await response.json();
+
+    return { status: response.status, data: res.data };
+  }
+}
