bc6f44ce8ce14e28d868db15ce17a092c84a8d5a
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-13 18:13:34 +0600

    Refactor component UserProfile

--- front/src/components/UserProfile.js
+++ front/src/components/UserProfile.js
@@ -1,41 +1,38 @@
 import React from 'react';
 
-class UserProfile extends React.Component {
-  render() {
-    const { isFetching, user } = this.props;
-    
+const UserProfile = (props) => {
+  const { isFetching, user } = props;
 
-    if(isFetching) {
-      return <div>Loading user profile...</div>
-    }
+  if(isFetching) {
+    return <div>Loading user profile...</div>
+  }
 
-    const { full } = user.data;
-    if(full) {
-      return (
-        <div>
-        <div>{ `${user.data.login}'s profile` }</div>
-        <img src={user.data.avatar} />
-        <ul>
-          <li>First name: {user.data.firstName}</li>
-          <li>Second name: {user.data.lastName}</li>
-          <li>Birthdate: {user.data.birthdate}</li>
-          <li>Gender: {user.data.gender}</li>
-          <li>Country: {user.data.country}</li>
-          <li>City: {user.data.city}</li>
-        </ul>
-      </div>
-      );
-    }
+  const { full } = user.data;
+  if(full) {
     return (
       <div>
-        <div>{ `${user.data.login}'s profile` }</div>
-        <ul>
-          <li>First name: {user.data.firstName}</li>
-          <li>Second name: {user.data.lastName}</li>
-        </ul>
-      </div>
+      <div>{ `${user.data.login}'s profile` }</div>
+      <img src={user.data.avatar} />
+      <ul>
+        <li>First name: {user.data.firstName}</li>
+        <li>Second name: {user.data.lastName}</li>
+        <li>Birthdate: {user.data.birthdate}</li>
+        <li>Gender: {user.data.gender}</li>
+        <li>Country: {user.data.country}</li>
+        <li>City: {user.data.city}</li>
+      </ul>
+    </div>
     );
   }
+  return (
+    <div>
+      <div>{ `${user.data.login}'s profile` }</div>
+      <ul>
+        <li>First name: {user.data.firstName}</li>
+        <li>Second name: {user.data.lastName}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default UserProfile;
