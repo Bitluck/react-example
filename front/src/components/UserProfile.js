cd5ce6df5ec2bead7b7921434336731f5cd29b3d
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-13 16:03:11 +0600

    Update component UserProfile
    Add new fields with user data

--- front/src/components/UserProfile.js
+++ front/src/components/UserProfile.js
@@ -2,18 +2,38 @@ import React from 'react';
 
 class UserProfile extends React.Component {
   render() {
-    const { isFetching, full, user } = this.props;
+    const { isFetching, user } = this.props;
+    
 
     if(isFetching) {
       return <div>Loading user profile...</div>
     }
+
+    const { full } = user.data;
     if(full) {
       return (
-        <div>{ `full ${user.data.login}` }</div>
+        <div>
+        <div>{ `${user.data.login}'s profile` }</div>
+        <img src={user.data.avatar} />
+        <ul>
+          <li>First name: {user.data.firstName}</li>
+          <li>Second name: {user.data.lastName}</li>
+          <li>Birthdate: {user.data.birthdate}</li>
+          <li>Gender: {user.data.gender}</li>
+          <li>Country: {user.data.country}</li>
+          <li>City: {user.data.city}</li>
+        </ul>
+      </div>
       );
     }
     return (
-      <div>{ `nefull ${user.data.login}` }</div>
+      <div>
+        <div>{ `${user.data.login}'s profile` }</div>
+        <ul>
+          <li>First name: {user.data.firstName}</li>
+          <li>Second name: {user.data.lastName}</li>
+        </ul>
+      </div>
     );
   }
 }
