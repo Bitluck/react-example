fb33c451b351179eec17dd219f20773f3ba03817
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-16 00:52:02 +0600

    Update auth saga for login request
    Add check for response status

--- front/src/sagas/auth.js
+++ front/src/sagas/auth.js
@@ -1,16 +1,23 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
+import { withRouter } from 'react-router-dom';
+
+import {
+  AUTH_LOGIN_REQUEST,
+  AUTH_LOGIN_SUCCESS } from '../constants/authActionTypes';
 import { loginRequest, loginSuccess, loginFailed } from '../actions/authActions';
 import AuthService from '../services/AuthService';
-
-import { AUTH_LOGIN_REQUEST } from '../constants/authActionTypes';
 
 const authService = new AuthService();
 
 function* loginUserRequest(action) {
   try {
     const user = yield call(authService.login, action.payload);
-    
-    yield put(loginSuccess({ status: user.status, data: user.data }));
+
+    if(user.status === 200) {
+      yield put(loginSuccess({ status: user.status, data: user.data }));
+    } else {
+      yield put(loginFailed(user.message));
+    }
   } catch(err) {
     yield put(loginFailed(err.message));
   }
