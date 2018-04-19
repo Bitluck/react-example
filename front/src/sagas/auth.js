0e223002117a756a852ecdc6cf10defc2a5fa40b
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-20 00:06:02 +0600

    Add saga method for user registration request

--- front/src/sagas/auth.js
+++ front/src/sagas/auth.js
@@ -1,15 +1,17 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { call, put, select, takeLatest } from 'redux-saga/effects';
 import { withRouter } from 'react-router-dom';
 
+import { AUTH_LOGIN_REQUEST, AUTH_LOGIN_SUCCESS, AUTH_REGISTER_REQUEST } from '../constants/authActionTypes';
 import {
-  AUTH_LOGIN_REQUEST,
-  AUTH_LOGIN_SUCCESS } from '../constants/authActionTypes';
-import { loginRequest, loginSuccess, loginFailed } from '../actions/authActions';
+  loginSuccess,
+  loginFailed,
+  registerSuccess,
+  registerFailed } from '../actions/authActions';
 import AuthService from '../services/AuthService';
 
 const authService = new AuthService();
 
-function* loginUserRequest(action) {
+function* loginUser(action) {
   try {
     const user = yield call(authService.login, action.payload);
 
@@ -23,6 +25,23 @@ function* loginUserRequest(action) {
   }
 }
 
+function* registerUser(action) {
+  try {
+    const user = yield call(authService.register, action. payload);
+
+    console.log({ user });
+
+    if(user.status === 201) {
+      yield put(registerSuccess({ data: user.data }));
+    } else {
+      yield put(registerFailed(user.message));
+    }
+  } catch(err) {
+      yield put(registerFailed(err.message));
+  }
+}
+
 export function* watchLoginSaga() {
-  yield takeLatest(AUTH_LOGIN_REQUEST, loginUserRequest);
+  yield takeLatest(AUTH_LOGIN_REQUEST, loginUser);
+  yield takeLatest(AUTH_REGISTER_REQUEST, registerUser);
 }
