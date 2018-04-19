30b75e22857f6b9ffa93cf1c3998725a191604ff
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-20 00:16:35 +0600

    Update rendered components in component Main

--- front/src/components/Main.js
+++ front/src/components/Main.js
@@ -2,17 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import LoginForm from '../components/LoginForm';
 import LoginFormContainer from '../containers/LoginFormContainer';
-import UserProfile from '../components/UserProfile';
+import RegisterFormContainer from '../containers/RegisterFormContainer';
 import UserProfileContainer from '../containers/UserProfileContainer';
-import UserProfilePage from '../containers/UserProfilePage';
 
 const Main = () => (
   <div className="main">
     <Switch>
-      <Route exact path='/' component={LoginForm} />
+      <Route exact path='/' component={LoginFormContainer} />
       <Route exact path='/login' component={LoginFormContainer} />
+      <Route exact path='/register' component={RegisterFormContainer} />
       <Route exact path='/users/:id' component={UserProfileContainer} />
       <Route path='*' render={(props) => <div>Note founde</div>} status='404'/>
     </Switch>
