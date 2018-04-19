37e2dcce14940efb1839148429cf93b42e44a39a
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-19 23:59:15 +0600

    Add attribute 'autoComplete' to component FormField

--- front/src/components/FormField.js
+++ front/src/components/FormField.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const FormField = props => {
-  const { input, label, type, meta: {touched, error, warning} } = props;
+  const { input, label, type, autoComplete, meta: {touched, error, warning} } = props;
 
   return (
     <div>
       <label>{label}</label>
-      <input {...input} placeholder={label} type={type} />
+      <input {...input} placeholder={label} type={type} autoComplete={autoComplete}/>
       { touched   &&
         ((error   && <span>{error}</span> ) ||
          (warning && <span>{warning}</span> )) }
