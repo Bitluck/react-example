4297a185cb40d102172993cf50a33de39eaf9ee9
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-21 16:14:30 +0600

    Replace 'console.log' logging to bunyan

--- src/index.js
+++ src/index.js
@@ -46,24 +46,16 @@ app.use(router.allowedMethods());
 
 const compiler = webpack(require('../webpack.config.js'), (err, stats) => {
   if (err || stats.hasErrors()) {
-    /* eslint-disable no-console */
-    console.log('There are webpack exception', err, stats.toJson('minimal'));
-    /* eslint-enable no-console */
+    logger.info('There are webpack exception', err, stats.toJson('minimal'));
     return;
   }
-
-  /* eslint-disable no-console */
-  console.log('webpack initialized successfully');
-  /* eslint-enable no-console */
+  
+  logger.info('webpack initialized successfully');
 });
 
 compiler.watch({}, () => {
-  /* eslint-disable no-console */
-  console.log('building...');
-  /* eslint-enable no-console */
+  logger.info('building...');
 });
 
 app.listen(port);
-/* eslint-disable no-console */
-console.log(`Server is started on ${port} port in ${configs.env}`);
-/* eslint-enable no-console */
+logger.info(`Server is started on ${port} port in ${configs.env}`);
