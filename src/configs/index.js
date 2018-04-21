0db334ad687ef93a1989158e60f54b749772628e
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-21 13:08:53 +0600

    Update app config
    Add ability change env arguments at start server

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,8 +1,13 @@
 const nconf = require('nconf');
 const path = require('path');
 
-nconf.argv().env(). file({ file: path.join(__dirname, 'config.json') });
+nconf.argv().env().file({ file: path.join(__dirname, 'config.json') });
 
-const configs = Object.assign(nconf.get('base'), nconf.get(nconf.get('NODE_ENV')));
+const env  = process.env.NODE_ENV || nconf.get('base:env');
+const name = process.env.APP_NAME || nconf.get('base:name');
+const host = process.env.APP_HOST || nconf.get('base:host');
+const port = process.env.APP_PORT || nconf.get('base:port');
+
+const configs = Object.assign(nconf.get('base'), nconf.get(nconf.get('NODE_ENV')), { env, name, host, port });
 
 module.exports = configs;
