a666f9bc1ed960cc41403651fbd783bc1a4c8760
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-24 12:03:40 +0600

    Fix default configs

--- src/configs/index.js
+++ src/configs/index.js
@@ -4,10 +4,11 @@ const path = require('path');
 nconf.argv().env().file({ file: path.join(__dirname, 'config.json') });
 
 const env  = process.env.NODE_ENV || nconf.get('base:env');
-const name = process.env.APP_NAME || nconf.get('base:name');
-const host = process.env.APP_HOST || nconf.get('base:host');
-const port = process.env.APP_PORT || nconf.get('base:port');
 
-const configs = Object.assign(nconf.get('base'), nconf.get(nconf.get('NODE_ENV')), { env, name, host, port });
+const configs = Object.assign(nconf.get('base'), nconf.get(env));
+
+configs.name = process.env.APP_NAME || configs.name;
+configs.host = process.env.APP_HOST || configs.host; 
+configs.port = process.env.APP_PORT || configs.port;
 
 module.exports = configs;
