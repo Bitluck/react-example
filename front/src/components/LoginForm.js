82013e2823a665d748554033fb2ef4967d479bad
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-25 19:28:32 +0600

    Refactor component LoginForm using bootstrap grid system

--- front/src/components/LoginForm.js
+++ front/src/components/LoginForm.js
@@ -1,37 +1,61 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
 import FormField from './FormField';
 
 import { loginRequest } from '../actions/authActions';
 import { required, maxLength, minLength } from '../utils/validate';
 
-import '../styles/components/LoginForm.css';
+import styles from '../styles/components/LoginForm.scss';
+import Redirect from 'react-router-dom/Redirect';
 
 const LoginForm = props => {
   const { handleSubmit, handleSubmitForm, dispatch, loginStatus, submitting } = props;
-  return (
-    <div className="login-form">
-      {loginStatus}
-      <form onSubmit={handleSubmit(async values => handleSubmitForm(values))}>
-        <Field
-          name="login"
-          type="text"
-          autoComplete="username"
-          component={FormField}
-          label="Login"
-          validate={[required, minLength(2), maxLength(20)]} />
 
-        <Field
-          name="password"
-          type="password"
-          autoComplete="current-password"
-          component={FormField}
-          label="Password"
-          validate={[required]} />
-          
-        <button disabled={submitting}>Submit</button>
+  if(props.loggedIn) {
+    return <Redirect to={"/"} />
+  }
+  return (
+    //<div className={styles.loginForm}>
+    <Grid fluid className={styles.loginForm}>
+      <form onSubmit={handleSubmit(async values => handleSubmitForm(values))}>
+        <Row>
+          <Col sm={12} className={styles.error}>
+            <span>{loginStatus}</span>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12}>
+            <Field
+              name="login"
+              type="text"
+              autoComplete="username"
+              component={FormField}
+              label="Login"
+              validate={[required, minLength(2), maxLength(20)]} />
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12}>
+            <Field
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              component={FormField}
+              label="Password"
+              validate={[required]} />
+          </Col>
+        </Row>
+        <Row>
+          <Col className={styles.button} sm={12}>
+            <button disabled={submitting}>Submit</button>
+          </Col>
+        </Row>
       </form>
-    </div>
+    </Grid>
+    //</div>
   );
 }
 
