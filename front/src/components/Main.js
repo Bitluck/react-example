5a18e3c5ac74e198a524e5d02e301ed19b14db3e
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-25 23:40:07 +0600

    Update routes in component Main

--- front/src/components/Main.js
+++ front/src/components/Main.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
-//import { Route } from 'react-router';
 
 import LoginFormContainer from '../containers/LoginFormContainer';
 import RegisterFormContainer from '../containers/RegisterFormContainer';
 import UserProfileContainer from '../containers/UserProfileContainer';
 import LogoutPage from '../components/LogoutPage';
 import LoginPage from '../views/LoginPage';
+import RegisterPage from '../views/RegisterPage';
 
 import { isAuth } from '../middleware/isAuth';
 
@@ -17,7 +17,7 @@ const Main = () => (
     <Switch>
       <Route exact path='/' component={RegisterFormContainer} />
       <Route exact path='/login' component={LoginPage} />
-      <Route exact path='/register' component={RegisterFormContainer} />
+      <Route exact path='/register' component={RegisterPage} />
       <Route exact path='/logout' component={LogoutPage} />
       <Route exact path='/users/:id' component={UserProfileContainer} />
       <Route path='*' render={(props) => <div>Note founde</div>} status='404'/>
