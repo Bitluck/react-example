9f7519a3dd21f6ef6f2f6639d048db96da750b91
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-25 19:32:05 +0600

    Add component LoginPage

---
+++ front/src/views/LoginPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
+import { isAuth } from '../middleware/isAuth';
+
+import LoginFormContainer from '../containers/LoginFormContainer';
+
+const LoginPage = props => {
+  return (
+    <Grid fluid>
+      <Row>
+        <Col sm={6}>
+          {'abc'}
+        </Col>
+        <Col sm={6}>
+          <LoginFormContainer />
+        </Col>
+      </Row>
+    </Grid>
+  );
+  }
+
+export default LoginPage;
