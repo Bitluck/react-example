88442c43f0bc3db2e571c90bab826d65f5638ee3
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-25 19:31:29 +0600

    Delete auth check middleware from auth routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,9 +3,7 @@
 const Router = require('koa-router');
 const auth = require('../controllers/auth');
 
-const { authCheck, unauthCheck } = require('../middlewares/auth-check');
-
 module.exports = new Router()
-  .post('/signin', unauthCheck, auth.signin)
-  .post('/signup', unauthCheck, auth.signup)
-  .post('/signout', authCheck, auth.signout);
+  .post('/signin', auth.signin)
+  .post('/signup', auth.signup)
+  .post('/signout', auth.signout);
