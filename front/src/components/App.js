e63c0086ab4654ca992c19476a0c3dad3fc48f26
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-25 19:25:58 +0600

    Refactor components using bootstrap grid system

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import '../styles/components/App.css';
 import Main from './Main';
 import Header from './Header';
 import Footer from './Footer';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
+import styles from '../styles/components/App.scss';
 
 class App extends React.Component {
   render() {
     return (
-      <div className='app'>
-        <Header />
-        <Main />
-        <Footer />
-      </div>
+      //<div className="app">
+      <Grid fluid className={styles.app}>
+        <Row>
+          <Header />
+        </Row>
+        <Row className={styles.content}>
+          <Col sm={2}>
+           {`text`}
+          </Col>
+          <Col sm={10}>
+            <Main />
+          </Col>
+        </Row>
+        <Row>
+          <Footer />
+        </Row>
+      </Grid>
+      //</div>
     );
   }
 }
--- front/src/components/FormField.js
+++ front/src/components/FormField.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
+import styles from '../styles/components/FormField.scss';
 
 const FormField = props => {
   const { input, label, type, autoComplete, meta: {touched, error, warning} } = props;
 
   return (
-    <div>
-      <label>{label}</label>
-      <input {...input} placeholder={label} type={type} autoComplete={autoComplete}/>
-      { touched   &&
-        ((error   && <span>{error}</span> ) ||
-         (warning && <span>{warning}</span> )) }
+    <div className={styles.formField}>
+        <Row>
+          <Col sm={4}>
+            <label>{label}</label>
+          </Col>
+          <Col sm={8}>
+            <input {...input}
+            className={styles['inp']}
+            placeholder={label}
+            type={type}
+            autoComplete={autoComplete}/>
+          </Col>
+        </Row>
+        <Row className={styles.inl}>
+          <Col sm={8} smOffset={4}>
+            { touched &&
+            ((error   && <span>{error}</span> ) ||
+            (warning  && <span>{warning}</span> )) }
+          </Col>
+        </Row>
     </div>
   );
 }
