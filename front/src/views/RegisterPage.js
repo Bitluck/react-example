30b478ab574934c3f3824c9a3a43b8de1ad3528d
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-25 23:42:24 +0600

    Add component RegisterPage

---
+++ front/src/views/RegisterPage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
+import { isAuth } from '../middleware/isAuth';
+
+import RegisterFormContainer from '../containers/RegisterFormContainer';
+
+import styles from '../styles/views/RegisterPage.scss';
+
+const RegisterPage = props => {
+  return (
+    <Grid fluid>
+      <Row>
+        <Col sm={6}>
+          {'abc'}
+        </Col>
+        <Col sm={6}>
+          <div className={styles.registerForm}>
+            <RegisterFormContainer />
+          </div>
+        </Col>
+      </Row>
+    </Grid>
+  );
+}
+
+export default RegisterPage;
