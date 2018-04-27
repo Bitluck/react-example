d79104c4e64b93a7d5634c55407ad9d95703b81d
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-27 18:19:05 +0600

    Add private routes in component Main

--- front/src/components/Main.js
+++ front/src/components/Main.js
@@ -2,22 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
 
+import PrivateRoute from './PrivateRoute';
 import LoginFormContainer from '../containers/LoginFormContainer';
 import RegisterFormContainer from '../containers/RegisterFormContainer';
 import UserProfileContainer from '../containers/UserProfileContainer';
 import LogoutPage from '../components/LogoutPage';
 import LoginPage from '../views/LoginPage';
 import RegisterPage from '../views/RegisterPage';
+import PostPage from '../views/PostPage';
 
-import { isAuth } from '../middleware/isAuth';
+import { isAuth, isUnauth } from '../middleware/isAuth';
 
 const Main = () => (
   <div className="main">
     {/* <div className="content-contaner"> */}
     <Switch>
-      <Route exact path='/' component={RegisterFormContainer} />
-      <Route exact path='/login' component={LoginPage} />
-      <Route exact path='/register' component={RegisterPage} />
+      <PrivateRoute exact path='/' auth={isAuth} component={PostPage} redirectPath={'/login'}/>
+      <PrivateRoute exact path='/login' auth={isUnauth} component={LoginPage} redirectPath={'/'}/>
+      <PrivateRoute exact path='/register' auth={isUnauth} component={RegisterPage} redirectPath={'/'}/>
+      <PrivateRoute exact path='/post' auth={isAuth} component={PostPage} redirectPath={'/'}/>
       <Route exact path='/logout' component={LogoutPage} />
       <Route exact path='/users/:id' component={UserProfileContainer} />
       <Route path='*' render={(props) => <div>Note founde</div>} status='404'/>
@@ -29,7 +32,5 @@ const Main = () => (
 Main.propType = {
   store: PropTypes.object.isRequired
 }
-
-
 
 export default Main;
