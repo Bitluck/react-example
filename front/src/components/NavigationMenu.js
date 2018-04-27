487688850b6d1e955b7933031e90272ec7286dae
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-27 17:58:12 +0600

    Add component NavigationMenu

---
+++ front/src/components/NavigationMenu.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
+import { NavLink } from 'react-router-dom';
+
+const NavigationMenu = props => {
+  return (
+    <Grid>
+      <Row>
+        <NavLink to={`/`}>Home (Feed)</NavLink>
+      </Row>
+      <Row>
+        <NavLink to={'/post'}>Create post</NavLink>
+      </Row>
+      <Row>
+        <NavLink to={'/friends'}>Friends</NavLink>
+      </Row>
+      <Row>
+        <NavLink to={`/users/1`}>Profile</NavLink>
+      </Row>
+      <Row>
+        <NavLink to={`/logout`}>Logout</NavLink>
+      </Row>
+    </Grid>
+  );
+}
+
+export default NavigationMenu;
