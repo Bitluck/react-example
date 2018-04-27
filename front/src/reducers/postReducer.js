f084373f32d6232e85d163f41e9e869a78db1949
Andrei Schukin <schukin_andrei@mail.ru>
2018-04-27 18:07:52 +0600

    Add case on changing picture in form to post reducer

--- front/src/reducers/postReducer.js
+++ front/src/reducers/postReducer.js
@@ -1,7 +1,10 @@
-import { POST_GET_REQUEST, POST_GET_SUCCESS } from '../constants/postActionTypes';
+import { POST_GET_REQUEST,
+         POST_GET_SUCCESS,
+         POST_PICTURE_CHOOSE } from '../constants/postActionTypes';
 
 const initialState = {
-  payload: {}
+  payload: {},
+  files: []
 }
 
 export default function postReducer(state = initialState, action) {
@@ -12,6 +15,8 @@ export default function postReducer(state = initialState, action) {
       return { ...state, payload, isFetching: true };
     case POST_GET_SUCCESS:
       return { ...state, payload, isFetching: false };
+    case POST_PICTURE_CHOOSE:
+      return { ...state, files: payload.files };
     default:
       return state;
   }
