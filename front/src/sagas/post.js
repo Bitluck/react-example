cf2e0c6978e5a8604924c9ebbaa06d44493d4bc0
Andrei Schukin <schukin_andrei@mail.ru>
2018-05-03 16:46:29 +0600

    Add saga methods for get posts

--- front/src/sagas/post.js
+++ front/src/sagas/post.js
@@ -1,7 +1,15 @@
 import { call, put, select, takeLatest } from 'redux-saga/effects';
 
-import { POST_CREATE_REQUEST } from '../constants/postActionTypes';
-import { postCreateRequest } from '../actions/postActions';
+import { POST_CREATE_REQUEST,
+         GET_USER_POSTS_REQUEST,
+         GET_FEED_POSTS_REQUEST,
+         GET_USER_MORE_POSTS_REQUEST,
+         GET_FEED_MORE_POSTS_REQUEST } from '../constants/postActionTypes';
+
+import { getUserPostsSuccess,
+         getUserPostsFailed,
+         getFeedPostsSuccess,
+         getFeedPostsFailed } from '../actions/postActions';
 import PostService from '../services/PostService';
 
 const postService = new PostService();
@@ -20,6 +28,34 @@ function* createPost(action) {
   }
 }
 
+function* getUserPosts(action) {
+  try {
+    const posts = yield call(postService.getUserAllPosts, action.payload);
+
+    console.log({ posts });
+
+    yield put(getUserPostsSuccess(posts));
+  } catch(err) {
+    yield put(getUserPostsFailed(err.message));
+  }
+}
+
+function* getFeedPosts(action) {
+  try {
+    const posts = yield call(postService.getFeedPosts, action.payload);
+
+    console.log({ posts });
+
+    yield put(getFeedPostsSuccess(posts));
+  } catch(err) {
+    yield put(getFeedPostsFailed(err.message));
+  }
+}
+
 export function* watchPostSaga() {
   yield takeLatest(POST_CREATE_REQUEST, createPost);
+  yield takeLatest(GET_USER_POSTS_REQUEST, getUserPosts);
+  yield takeLatest(GET_USER_MORE_POSTS_REQUEST, getUserPosts);
+  yield takeLatest(GET_FEED_POSTS_REQUEST, getFeedPosts);
+  yield takeLatest(GET_FEED_MORE_POSTS_REQUEST, getFeedPosts);
 }
