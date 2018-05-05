6ec44021f48e3adbcb7bdf627bb644b586b8ff41
Andrei Schukin <schukin_andrei@mail.ru>
2018-05-05 22:17:40 +0600

    Fix user exists checking in routes

--- src/routes/friends.js
+++ src/routes/friends.js
@@ -4,12 +4,13 @@ const Router = require('koa-router');
 const friendsController = require('../controllers/friends');
 
 const { authCheck } = require('../middlewares/auth-check');
+const selfUser = require('../middlewares/same-user');
 const { existsUser } = require('../middlewares/exists-user');
 
 module.exports = new Router()
   .get('/', authCheck, existsUser, friendsController.getAllFriends)
   .post('/', authCheck, existsUser, friendsController.makeFriends)
   .delete('/', authCheck, friendsController.deleteFromFriends)
-  .get('/requests/in', authCheck, existsUser, friendsController.getInRequests)
-  .get('/requests/out', authCheck, existsUser, friendsController.getOutRequests)
+  .get('/requests/in', authCheck, selfUser, existsUser, friendsController.getInRequests)
+  .get('/requests/out', authCheck, selfUser, existsUser, friendsController.getOutRequests)
   .get('/relation', authCheck, existsUser, friendsController.isRelation);
