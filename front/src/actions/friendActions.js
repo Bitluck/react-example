94923d54b8252711ec8a4c6e5dd08726bc61d9b0
Andrei Schukin <schukin_andrei@mail.ru>
2018-05-06 15:04:29 +0600

    Add new friend actions for get friends

--- front/src/actions/friendActions.js
+++ front/src/actions/friendActions.js
@@ -1,7 +1,13 @@
 import { GET_FRIEND_RELATION_REQUEST,
          GET_FRIEND_RELATION_SUCCESS,
          MAKE_FRIENDS_REQUEST,
-         MAKE_FRIENDS_SUCCESS } from '../constants/friendActionTypes';
+         MAKE_FRIENDS_SUCCESS,
+         GET_FRIENDS_REQUEST,
+         GET_IN_REQUESTS_REQUEST,
+         GET_OUT_REQUESTS_REQUEST,
+         GET_FRIENDS_SUCCESS,
+         GET_FRIENDS_FAILED,
+         CHANGE_SELECTED_FRIENDS_TAB } from '../constants/friendActionTypes';
 
 export function getFriendRelationRequest(userId) {
   return {
@@ -28,5 +34,47 @@ export function makeFriendsSuccess(result) {
   return {
     type: MAKE_FRIENDS_SUCCESS,
     payload: { result }
+  }
+}
+
+export function getFriendsRequest(userId) {
+  return {
+    type: GET_FRIENDS_REQUEST,
+    payload: { userId }
+  }
+}
+
+export function getInRequests(userId) {
+  return {
+    type: GET_IN_REQUESTS_REQUEST,
+    payload: { userId }
+  }
+}
+
+export function getOutRequests(userId) {
+  return {
+    type: GET_OUT_REQUESTS_REQUEST,
+    payload: { userId }
+  }
+}
+
+export function getFriendsSuccess(friendsData) {
+  return {
+    type: GET_FRIENDS_SUCCESS,
+    payload: { friendsData }
+  }
+}
+
+export function getFriendsFailed(msg) {
+  return {
+    type: GET_FRIENDS_FAILED,
+    payload: { msg }
+  }
+}
+
+export function changeSelectedFriendsTab(selectedTab, tabNamespace, userId) {
+  return {
+    type: CHANGE_SELECTED_FRIENDS_TAB,
+    payload: { tab: selectedTab, namespace: tabNamespace, id: userId }
   }
 }
