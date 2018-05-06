4e4a848e30f2e5db75695181965ee054f4dd4fae
Andrei Schukin <schukin_andrei@mail.ru>
2018-05-06 23:45:23 +0600

    Change order of server ports

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const logger = require('./configs/logger');
 const logMiddleware = require('./middlewares/log');
 const responseHandler = require('./middlewares/response-handler');
 
-const port = configs.port || process.env.PORT || 3000;
+const port = process.env.PORT || configs.port || 3000;
 process.env.PORT = port;
 const host = configs.host || '0.0.0.0';
 const app = new Koa();
