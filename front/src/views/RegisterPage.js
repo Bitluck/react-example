b5ab7eb18d3e3398531b5612d133dfb6a6d75287
Andrei Schukin <schukin_andrei@mail.ru>
2018-05-08 17:39:46 +0600

    Add text to RegisterPage

--- front/src/views/RegisterPage.js
+++ front/src/views/RegisterPage.js
@@ -2,11 +2,10 @@ import React from 'react';
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
-
-import { isAuth } from '../middleware/isAuth';
 import { Helmet } from 'react-helmet';
 
 import RegisterFormContainer from '../containers/RegisterFormContainer';
+import { isAuth } from '../middleware/isAuth';
 
 import styles from '../styles/views/RegisterPage.scss';
 
@@ -17,8 +16,13 @@ const RegisterPage = props => {
         <title>Registration - Social Network</title>
       </Helmet>
       <Row>
-        <Col sm={6}>
-          {'abc'}
+        <Col sm={6} className={styles.registerText}>
+          <p>Registering you get opportunities:</p>
+          <ul>
+            <li>make friends on interests;</li>
+            <li>share your thinks with friends;</li>
+            <li>see than friends shared.</li>
+          </ul>
         </Col>
         <Col sm={6}>
           <div className={styles.registerForm}>
