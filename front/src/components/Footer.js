c2dca11d57dd309402ad3f845cfcab03af69be67
Andrei Schukin <schukin_andrei@mail.ru>
2018-05-08 17:20:17 +0600

    Add content in Footer

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { range } from 'lodash';
+import { Link } from 'react-router-dom';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
 import styles from '../styles/components/Footer.scss';
 
 class Footer extends React.Component {
   render() {
     return (
-      <div className={styles.footer}>
-        Footer
-      </div>
+      <Grid className={styles.footer}>
+        <Row>
+          <Col sm={4}>
+            {`(c) ${new Date().getFullYear()} · SocialNet`}
+          </Col>
+          <Col sm={4}>
+            <Link to='/'>
+              <img src="/img/avatars/default_small.png" width='30px' height='30px' />
+            </Link>
+          </Col>
+          <Col sm={4}>
+            <a href="mailto:bitluck11@gmail.com">Contact the author</a>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
