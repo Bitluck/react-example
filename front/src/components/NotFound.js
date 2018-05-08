b24c1f1e614f6468385a5968400466dc0ca18993
Andrei Schukin <schukin_andrei@mail.ru>
2018-05-08 17:16:18 +0600

    Add picture to not found page

--- front/src/components/NotFound.js
+++ front/src/components/NotFound.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
+import styles from '../styles/components/NotFound.scss';
 
 const NotFound = props => {
-  return <div>Not Found Page</div>
+  return (
+  <Grid fluid>
+    <Row className={styles.notFoundText}>
+      Not Found Page
+    </Row>
+    <Row className={styles.notFoundHiddenText}>
+      Who you gonna call? ;)
+    </Row>
+    <Row>
+      <img src="/img/not_found/404.png" alt="not found" className={styles.notFoundPicture} />
+    </Row>
+  </Grid>
+  );
 }
 
 export default NotFound;
