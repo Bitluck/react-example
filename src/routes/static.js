e933b9581ec746288b685163f2fae2b1128cc281
Andrei Schukin <schukin_andrei@mail.ru>
2018-05-08 21:40:06 +0600

    Fix static router

--- src/routes/static.js
+++ src/routes/static.js
@@ -6,6 +6,16 @@ const { createReadStream } = require('fs');
 
 module.exports = new Router()
   .get('*', async ctx => {
+    if(ctx.request.path === '/dist/bundle.js') {
+      ctx.type = 'text/javascript';
+      ctx.status = 200;
+      ctx.body = createReadStream(path.resolve('./public/dist/bundle.js'));
+    }
+    if(ctx.request.path === '/dist/bundle.css') {
+      ctx.type = 'text/css';
+      ctx.status = 200;
+      ctx.body = createReadStream(path.resolve('./public/dist/bundle.css'));
+    }
     if(ctx.request.path !== '/favicon.ico') {
       ctx.type = 'html';
       ctx.status = 200;
